2ebe48f0fa41c02f0540b0cda4aa80870d5042df
ReynaEduardo23 <2523160117@e.uttecamac.edu.mx>
2025-10-19 15:37:03 -0600

    Primer commit de la calculadora con CI

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+package Calculadora;
+public class Calculadora {
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        if (b == 0) throw new ArithmeticException("No se puede dividir entre cero");
+        return a / b;
+    }
+}
---
+++ src/Calculadora/test/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package Calculadora.test;
+
+import Calculadora.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        assert Calculadora.sumar(2, 3) == 5 : "Error en suma";
+        assert Calculadora.restar(5, 3) == 2 : "Error en resta";
+        assert Calculadora.multiplicar(2, 4) == 8 : "Error en multiplicación";
+        assert Calculadora.dividir(10, 2) == 5 : "Error en división";
+
+        System.out.println(" Todas las pruebas pasaron correctamente.");
+    }
+}
